395dcaf7bf400b50fab722ee41c23f68b82e3b56
Madhav <spmadhav04@gmail.com>
2026-07-12 01:38:09 +0530

    fix: specify dotenv config path for environment variables in s3.js and worker.js

--- apps/worker/src/worker.js
+++ apps/worker/src/worker.js
@@ -1,4 +1,9 @@
-require("dotenv").config();
+const path = require("path");
+
+require("dotenv").config({
+  path: path.resolve(__dirname, "../.env"),
+});
+
 
 const fs = require("fs");
 const { Worker } = require("bullmq");
